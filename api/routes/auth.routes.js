dd2f36b952a350aeca902ebe527c02810a0d32f1
limjens <limjens49@gmail.com>
2026-05-18 05:31:24 +0800

    updated auth routh to support flet pos

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -5,9 +5,16 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+
+// Protected user management routes
+router.use(authMiddleware);
+router.get("/users", authController.getAll);
+router.put("/users/:username", authController.update);
+router.delete("/users/:username", authController.delete);
 
 module.exports = router;
